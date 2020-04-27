6f41c1c9a3322d6092694ebad7b02af917056652
Jamie Epstein <jasepstein@gmail.com>
2020-04-27 11:15:08 -0500

    Added package.json

--- scraper.js
+++ scraper.js
@@ -1,14 +1,25 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cron = require('node-cron');
 
 const url = 'https://store.us.asmodee.com/catalogue/blue-moon-city_1952/';
 
+cron.schedule('*/5 * * * * *', () => {
+    console.log('BOOP');
+})
+
 axios(url)
+    //Parses the DOM elements and returns the stock status of the item
     .then(response => {
         const html = response.data;
         const $ = cheerio.load(html);
         const stockStatus = $('.mod-product-details-submit').text();
 
-        console.log(stockStatus);
+        //console.log(stockStatus);
+        return stockStatus;
+    })
+    .then(status => {
+        console.log();
     })
     .catch(console.error);
+
